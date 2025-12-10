fcb24b4ce0a1ff99649b3da5bc00ef5ff5a78700
Javon <jhyche80@gmail.com>
2025-12-10 12:27:27 -0500

    I made the basic add task for the dashboard app

---
+++ src/Components/Dashboard/Dashboard.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import RenderList from '../RenderList/RenderList'
+
+export default function Dashboard() {
+  return (
+    <div>
+      <RenderList/>
+    </div>
+  )
+}
--- src/Components/RenderList/RenderList.tsx
+++ src/Components/RenderList/RenderList.tsx
@@ -1,98 +1,122 @@
 import React from 'react'
-import type { Task,TaskStatus, TaskCat, TaskPrio } from '../types'
+import type { Task, TaskStatus, TaskCat, TaskPrio } from '../types'
 import { useState } from 'react'
 import TaskList from '../TaskList/TaskList'
 import TaskFilter from '../TaskFilter/TaskFilter'
 
 export default function RenderList() {
-    const [task, setTask]= useState<Task[]>([])
-    const[date,setDate] = useState("")
-    const [title,setTitle]= useState('')
-    const [description,setDescription]=useState('')
-    const [priority,setPriority]= useState<TaskPrio>('low')
-    const[category,setCategory]=useState<TaskCat>('personal')
-    const[time,setTime]= useState('')
+    const [task, setTask] = useState<Task[]>([])
+    const [date, setDate] = useState("")
+    const [title, setTitle] = useState('')
+    const [description, setDescription] = useState('')
+    const [priority, setPriority] = useState<TaskPrio>('low')
+    const [category, setCategory] = useState<TaskCat>('personal')
+    const [time, setTime] = useState('')
+    const [error, setError] = useState('')
     const [filters, setFilters] = useState<{
-    status?: TaskStatus
-    priority?: 'low' | 'medium' | 'high'
-  }>({})
-   const handleFilterChange = (newfilters: any) => {
-    setFilters(newfilters)
-  }
-   const handleStatusChange = (taskid: string, newStatus: TaskStatus) => {
-    setTask(prev => prev.map(task => task.id === taskid ? { ...task, status: newStatus } : task))
+        status?: TaskStatus
+        priority?: 'low' | 'medium' | 'high'
+    }>({})
+    const handleFilterChange = (newfilters: any) => {
+        setFilters(newfilters)
+    }
+    const handleStatusChange = (taskid: string, newStatus: TaskStatus) => {
+        setTask(prev => prev.map(task => task.id === taskid ? { ...task, status: newStatus } : task))
 
-  }
-    const handleTask = ()=>{
-        const newTask:Task={
+    }
+    const handleTask = () => {
+        const newTask: Task = {
             id: Date.now().toString(),
-            title, 
-            description, 
+            title,
+            description,
             priority,
-            dueDate:date, 
+            dueDate: date,
             time,
             category,
-            status:'pending'
-        
+            status: 'pending'
+
         }
-        setTask(prev=>[...prev,newTask])
+        if (!title.trim()) {
+            setError("Please enter a Task!")
+            return
+        }
+        if (!description.trim()) {
+            setError('Please enter a description!')
+            return
+        }
+        if (!date) {
+            setError("Please enter a due date!")
+            return
+        }
+        if(!time){
+            setError("Please enter a time!")
+        }
+        setTask(prev => [...prev, newTask])
+        
+        setTitle('')
+        setDescription('')
+        setDate('')
+        setPriority('low')
+        setCategory('personal')
+        setTime('')
     }
-    const filteredTask = task.filter(task=>{
+    const filteredTask = task.filter(task => {
         if (filters.status && task.status !== filters.status) return false
-    if (filters.priority && task.priority !== filters.priority) return false
-    return true
+        if (filters.priority && task.priority !== filters.priority) return false
+        return true
     })
-    const handleDelete=(taskid:string) =>{
-        setTask(prev => prev.filter(task=> task.id !== taskid))
+    const handleDelete = (taskid: string) => {
+        setTask(prev => prev.filter(task => task.id !== taskid))
 
     }
-   
-  return (
-    <div>
-        <h2>Enter information here:</h2>
-      <input type="text" 
-      value={title}
-      onChange={(e)=>setTitle(e.target.value)}
-      />
-      <textarea 
-      name="Description"
-       id="Description"
-        maxLength={30}
-        placeholder='Task description max 30 characters '
-        onChange={(e)=> setDescription(e.target.value)}
-        />
-      <input type="date" value={date} 
-      onChange={(e)=> setDate(e.target.value)}
-      />
-      <input type="time" 
-      value={time} 
-      onChange={(e)=> setTime(e.target.value)}
-       />
-      <select name="Cat" id="Cat" value={category}
-      onChange={(e)=>setCategory(e.target.value as TaskCat)}
-      >
-        <option value="school">School</option>
-        <option value="work">Work</option>
-        <option value="personal">Personal</option>
-        <option value="fitness">Fitness</option>
-        <option value="finance">Finance</option>
-        <option value="other">Other</option>
-      </select>
-      <select name="Priority" id="Priority"
-      value={priority} 
-      onChange={(e)=>setPriority(e.target.value as TaskPrio)}>
-        <option value="low">Low</option>
-        <option value="medium">Medium</option>
-        <option value="high">High</option>
-      </select>
-      <button onClick={handleTask}>Add task</button>
 
-    <TaskFilter onFilterChange={handleFilterChange}/>
-    <TaskList
-    tasks={filteredTask}
-    onStatusChange={handleStatusChange}
-    onDelete={handleDelete}
-    />
-    </div>
-  )
+    return (
+        <div>
+            <h2>Enter information here:</h2>
+            {error && <p style={{ color: 'red' }}>{error}</p>}
+            <input type="text"
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+            />
+            <textarea
+                name="Description"
+                id="Description"
+                maxLength={30}
+                placeholder='Task description max 30 characters '
+                onChange={(e) => setDescription(e.target.value)}
+            />
+            <input type="date" value={date}
+                onChange={(e) => setDate(e.target.value)}
+            />
+            <input type="time"
+                value={time}
+                onChange={(e) => setTime(e.target.value)}
+            />
+            <select name="Cat" id="Cat" value={category}
+                onChange={(e) => setCategory(e.target.value as TaskCat)}
+            >
+                <option value="school">School</option>
+                <option value="work">Work</option>
+                <option value="personal">Personal</option>
+                <option value="fitness">Fitness</option>
+                <option value="finance">Finance</option>
+                <option value="other">Other</option>
+            </select>
+            <select name="Priority" id="Priority"
+                value={priority}
+                onChange={(e) => setPriority(e.target.value as TaskPrio)}>
+                <option value="low">Low</option>
+                <option value="medium">Medium</option>
+                <option value="high">High</option>
+            </select>
+            <button onClick={handleTask}>Add task</button>
+
+            <TaskFilter onFilterChange={handleFilterChange} />
+            <TaskList
+                tasks={filteredTask}
+                onStatusChange={handleStatusChange}
+                onDelete={handleDelete}
+            />
+        </div>
+    )
 }
