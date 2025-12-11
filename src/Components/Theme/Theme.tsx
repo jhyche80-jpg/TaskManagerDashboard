adbb0cd845628764cb7ca1672d4da22de1545313
Javon <jhyche80@gmail.com>
2025-12-11 09:20:35 -0500

    I added a light dark theme to the page

---
+++ src/Components/Theme/Theme.tsx
@@ -0,0 +1,27 @@
+import { createContext, useContext, useState } from "react";
+import type { Theme, ThemeContextType } from "../types";
+
+
+const ThemeContext = createContext<ThemeContextType | null>(null);
+
+export function ThemeProvider({ children }: { children: React.ReactNode }) {
+  const [theme, setTheme] = useState<Theme>("light");
+
+  const toggleTheme = () => {
+    setTheme(prev => (prev === "light" ? "dark" : "light"));
+  };
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      <div className={theme}>{children}</div>
+    </ThemeContext.Provider>
+  );
+}
+
+export function useTheme() {
+  const context = useContext(ThemeContext);
+  if (!context) {
+    throw new Error("useTheme must be used inside ThemeProvider");
+  }
+  return context;
+}
